53deb2ef98d95f3acf621b51eeb84319300cd531
Nikolay Tuzov <just.skiv@gmail.com>
2013-04-17 01:34:34 +0400

    Метод Монте-Карло

--- Main.java
+++ Main.java
@@ -1,7 +1,73 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-      Polymer polymer = new Polymer(4);
+  public static void main(String[] args) {
+    int[] basicCoordinates = new int[]{0, 0, 0};
+
+
+    ArrayList<ArrayList<int[]>> polymers = Polymer.getRandomPolymersSet(basicCoordinates, 3, 20);
+
+    for (int i = 0; i < polymers.size(); i++) {
+      //System.out.println("\nPolymer №: " + i);
+
+      for (int[] point : polymers.get(i)) {
+        //System.out.println(Arrays.toString(point) + " ");
+
+      }
+
     }
+
+    System.out.println("Polymers NotIntersected count: " + Polymer.getNotIntersectedCountInManyPolymers(polymers));
+    System.out.println("Polymers average radius: " + Polymer.getAverageRadius(polymers));
+    System.out.println("Polymers count: " + polymers.size());
+
+    /*ArrayList<int[]> tPolymer = Polymer.getRandomPolymer(basicCoordinates, 4);
+
+    for (int[] point : tPolymer) {
+      System.out.println(Arrays.toString(point) + " ");
+
+    }*/
+
+    //ArrayList<ArrayList<int[]>> polymers = Polymer.buildWholeSet(basicCoordinates, 5);
+
+    /*System.out.println("Polymers ints count: " + Polymer.getNotIntersectedCountInManyPolymers(polymers));
+    System.out.println("Polymers average radius: " + Polymer.getAverageRadius(polymers));
+    System.out.println("Polymers count: " + polymers.size());*/
+
+
+
+    /*for (int i = 0; i < polymers.size(); i++) {
+      System.out.println("\nPolymer №: " + i);
+
+      for (int[] point : polymers.get(i)) {
+        System.out.println(Arrays.toString(point) + " ");
+
+      }
+
+    }*/
+
+
+      /*for (int[] point : polymers.get(34)) {
+        System.out.println(Arrays.toString(point) + " ");
+
+      }*/
+
+
+    //ArrayList<int[]> polymer = Polymer.getFoundation(basicCoordinates);
+
+    /*for(Iterator<int[]> i = polymer.iterator(); i.hasNext(); ) {
+      int[] item = i.next();
+      int[] test = new int[]{1,3,4};
+      System.out.println(Arrays.toString(test));
+    }*/
+
+
+    /* polymer = Polymer.buildPointByDirectionNumber(polymer, 0, 0);
+    for (int[] point : polymer) {
+      System.out.println(Arrays.toString(point) + "\n");
+
+    }*/
+  }
 }
--- Polymer.java
+++ Polymer.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,35 +10,26 @@ import java.util.ArrayList;
  * To change this template use File | Settings | File Templates.
  */
 public class Polymer {
-  public int maxLength, currentLenght = 0;
-  public int[][] coordinates;
-
-  public Polymer(int tLength) {
-    maxLength = tLength;
-    coordinates = new int[maxLength][3];
-
-    coordinates[0] = new int[]{0, 0, 0};
-    coordinates[1] = new int[]{0, 2, 4};
-
-    currentLenght = 1;
-
-    //System.out.println(Arrays.deepToString(coordinates));
-    //System.out.println(coordinates.length);
-  }
 
   /**
-   * Строит одно звено по номеру (номера для удобства построения циклом)
+   * Строит одну точку по номеру направления (номера нужны для удобства построения циклом)
    *
+   * @param polymer   Массив, описывающий полимер
+   * @param fromPoint Точка от которой будем строить
    * @param direction В какую сторону строить (0-5: x, -x, y, -y, z, -z)
-   * @param from      Точка от которой будем строить
    */
-  public void buildByNumber(int direction, int[] from) {
+  public static ArrayList<int[]> buildPointByDirectionNumber(ArrayList<int[]> polymer, int fromPoint, int direction) {
     //Если понадобиться размещение построение из произвольной точки цепи,
     //то просто передавать значение position через параметр функции
+    //int position = polymer.size();
 
-    int[] newPoint = from;
+    ArrayList<int[]> tPolymer = new ArrayList<int[]>();
+    tPolymer.addAll(polymer);
 
-    int position = currentLenght;
+    int[] currentPoint = polymer.get(fromPoint);
+    int[] newPoint = new int[3];
+    System.arraycopy(currentPoint, 0, newPoint, 0, currentPoint.length);
+
     switch (direction) {
       case 0:
         newPoint[0]++;
@@ -58,35 +51,214 @@ public class Polymer {
         break;
     }
 
-    coordinates[position] = newPoint;
+    tPolymer.add(newPoint);
 
-    currentLenght++;
-
+    return tPolymer;
   }
 
   /**
    * Строит все возможное множество цепочек заданной длины
    *
-   * @param lenght Максимальная длина цепочек
+   * @param length Максимальная длина цепочек
    * @return
    */
-  public static ArrayList buildWholeSet(int lenght) {
-    Polymer foundation = new Polymer(lenght);
+  public static ArrayList<ArrayList<int[]>> buildWholeSet(int[] coordinates, int length) {
+    ArrayList<int[]> foundation = Polymer.getFoundation(coordinates);
 
-    ArrayList result = buildSetFromPoint(foundation[0], lenght);
+    ArrayList<ArrayList<int[]>> tResultPolymers = new ArrayList<ArrayList<int[]>>();
 
+    ArrayList<ArrayList<int[]>> resultPolymers = buildSetPolymersFromPoint(foundation, 0, length, tResultPolymers);
+
+    return resultPolymers;
   }
-
 
 
   /**
-   * Строит все возможное множество цепочек заданной длины из указанной точки
+   * Достраивает все возможное множество цепочек заданной длины из указанной точки
    *
-   * @param point Точка, из которой начинается построение
-   * @param length Длина
+   * @param polymer
+   * @param fromPoint Точка, из которой начинается построение
+   * @param length    Длина
    * @return
    */
-  public static ArrayList buildSetFromPoint(int[] point, int length) {
+  public static ArrayList<ArrayList<int[]>> buildSetPolymersFromPoint(ArrayList<int[]> polymer, int fromPoint, int length, ArrayList<ArrayList<int[]>> resultPolymers) {
 
+    if (polymer.size() - 1 != length) {
+      for (int i = 0; i <= 5; i++) {
+        ArrayList<int[]> tPolymer = new ArrayList<int[]>();
+        tPolymer.addAll(polymer);
+
+        tPolymer = Polymer.buildPointByDirectionNumber(tPolymer, fromPoint, i);
+
+        Polymer.buildSetPolymersFromPoint(tPolymer, tPolymer.size() - 1, length, resultPolymers);
+      }
+    } else {
+      resultPolymers.add(polymer);
+    }
+
+    return resultPolymers;
   }
+
+
+  /**
+   * Создает полимер, состоящий из одной точки (условно - "базовая точка")
+   *
+   * @param coordinates
+   * @return
+   */
+  public static ArrayList<int[]> getFoundation(int[] coordinates) {
+    ArrayList<int[]> foundation = new ArrayList<int[]>();
+    foundation.add(coordinates);
+
+    return foundation;
+  }
+
+  /**
+   * Количество самопересечений в полимере
+   *
+   * @param polymer
+   * @return
+   */
+  public static int getIntersectionsCountInOnePolymer(ArrayList<int[]> polymer) {
+    int result = 0;
+
+    for (int i = 0; i < polymer.size(); i++) {
+
+      for (int j = i + 1; j < polymer.size(); j++) {
+
+        int[] pointA = polymer.get(i);
+        int[] pointB = polymer.get(j);
+
+        if (Arrays.equals(pointA, pointB)) result++;
+
+      }
+
+    }
+
+
+    return result;
+  }
+
+  /**
+   * Общее количество самопересечений в наборе полимеров
+   *
+   * @param polymers
+   * @return
+   */
+  public static int getIntersectionsCountInManyPolymers(ArrayList<ArrayList<int[]>> polymers) {
+    int result = 0;
+    for (ArrayList<int[]> polymer : polymers) {
+      result += Polymer.getIntersectionsCountInOnePolymer(polymer);
+    }
+
+    return result;
+  }
+
+  /**
+   * Проверяет, не является ли полимер самоНЕпересекающимся
+   *
+   * @param polymer
+   * @return
+   */
+  public static boolean isNotIntersected(ArrayList<int[]> polymer) {
+    boolean notIntersected = true;
+
+    for (int i = 0; i < polymer.size(); i++) {
+
+      for (int j = i + 1; j < polymer.size(); j++) {
+
+        int[] pointA = polymer.get(i);
+        int[] pointB = polymer.get(j);
+
+        if (Arrays.equals(pointA, pointB)) {
+          notIntersected = false;
+          break;
+
+        }
+      }
+
+      if (!notIntersected) break;
+
+    }
+
+
+    return notIntersected;
+  }
+
+  /**
+   * Возвращает количество самоНЕпересекающихся полимеров из набора
+   *
+   * @param polymers
+   * @return
+   */
+  public static int getNotIntersectedCountInManyPolymers(ArrayList<ArrayList<int[]>> polymers) {
+    int result = 0;
+    for (ArrayList<int[]> polymer : polymers) {
+      if (Polymer.isNotIntersected(polymer))
+        result++;
+    }
+
+    return result;
+  }
+
+
+  /**
+   * Возвращает средний квадрат расстояния между первым и последним узлами полимеров из набора
+   *
+   * @param polymers
+   * @return
+   */
+  public static float getAverageRadius(ArrayList<ArrayList<int[]>> polymers) {
+    float result = 0;
+
+    for (ArrayList<int[]> polymer : polymers) {
+      int lastPointNumber = polymer.size() - 1;
+      int[] lastPoint = polymer.get(lastPointNumber);
+
+      int x = lastPoint[0];
+      int y = lastPoint[0];
+      int z = lastPoint[0];
+
+      float radiusSquared = x * x + y * y + z * z;
+
+      result += radiusSquared;
+    }
+
+    result = result / polymers.size();
+
+    return result;
+  }
+
+  /**
+   * Возвращает случайный полимер
+   *
+   * @param foundationCoordinates
+   * @param length
+   * @return
+   */
+  public static ArrayList<int[]> getRandomPolymer(int[] foundationCoordinates, int length) {
+    ArrayList<int[]> resultPolymer = Polymer.getFoundation(foundationCoordinates);
+
+    final Random generator = new Random();
+    for (int i = 1; i < length; i++) {
+      int directionNumber = generator.nextInt(5);
+
+      resultPolymer = Polymer.buildPointByDirectionNumber(resultPolymer, resultPolymer.size() - 1, directionNumber);
+    }
+
+
+    return resultPolymer;
+  }
+
+  public static ArrayList<ArrayList<int[]>> getRandomPolymersSet(int[] foundationCoordinates, int length, int count) {
+    ArrayList<ArrayList<int[]>> resultPolymers = new ArrayList<ArrayList<int[]>>();
+
+    for (int i = 0; i < count; i++) {
+      ArrayList<int[]> tPolymer = Polymer.getRandomPolymer(foundationCoordinates, length);
+      resultPolymers.add(tPolymer);
+    }
+
+    return resultPolymers;
+  }
+
 }
